b6495d9dd39f8ed60cc7de442075311379968584
lbilde <ljb@easv.dk>
2018-02-20 20:55:08 +0100

    adding dynamic routing to our sidenav with a ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  routes = [
+    {route: '/', title: 'Home', icon: 'home'},
+    {route: '/albums', title: 'Albums', icon: 'folder'},
+    {route: '/users', title: 'Users', icon: 'event'}
+  ];
   navBarOpen = true;
 
   toggleNav() {
