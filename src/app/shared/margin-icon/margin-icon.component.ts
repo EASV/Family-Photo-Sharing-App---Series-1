7780d5ea35b0b23f50e81fc8faf3fcdcb55ddeac
lbilde <ljb@easv.dk>
2018-02-20 10:34:26 +0100

    adding margin-icon

---
+++ src/app/shared/margin-icon/margin-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-margin-icon',
+  templateUrl: './margin-icon.component.html',
+  styleUrls: ['./margin-icon.component.css']
+})
+export class MarginIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
