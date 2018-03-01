cdc9665e785bad381563fb5787c2b4e47d4f0be9
lbilde <ljb@easv.dk>
2018-03-01 08:39:25 +0100

    added Auth Guard, incl small bug fix and signup

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { User } from './user';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +16,14 @@ export class AuthService {
 
   logout(): Promise<any> {
     return this.fireAuth.auth.signOut();
+  }
+
+  signup(user: User): Promise<any> {
+    return this.fireAuth.auth
+      .createUserAndRetrieveDataWithEmailAndPassword(
+        user.email,
+        user.password
+      );
   }
 
   isAuthenticated(): Observable<boolean> {
