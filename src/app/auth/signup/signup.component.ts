8e33e94e09d5b143046b3d7c0b3d3e7b6874c1f9
lbilde <ljb@easv.dk>
2018-03-01 11:16:31 +0100

    adding validation to signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../shared/auth.service';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
@@ -18,9 +18,9 @@ export class SignupComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = this.fb.group({
-      email: '',
-      password: '',
-      repeatPassword: ''
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      repeatPassword: ['', [Validators.required]]
     });
   }
 
