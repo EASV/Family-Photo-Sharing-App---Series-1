cad9e33b90aa6f0d8ed0103b827defd54f7e4c0c
lbilde <ljb@easv.dk>
2018-03-15 12:20:05 +0100

    adding ui to file details

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileDetailsComponent } from './file-details/file-details.component';
 import { FileSystemContainerComponent } from './file-system-container/file-system-container.component';
+import { MatButtonModule, MatCardModule, MatIconModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [FileDetailsComponent, FileSystemContainerComponent],
   providers: [],
