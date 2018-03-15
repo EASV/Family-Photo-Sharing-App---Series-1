5380f63bb1b02f39120b24770e1b38eecf818d91
lbilde <ljb@easv.dk>
2018-03-15 10:52:40 +0100

    using file-system in albums

--- src/app/albums/albums.module.ts
+++ src/app/albums/albums.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumsListComponent } from './albums-list/albums-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { FileSystemModule } from '../file-system/file-system.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FileSystemModule
   ],
   declarations: [AlbumsListComponent]
 })
