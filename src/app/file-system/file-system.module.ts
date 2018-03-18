dbc4860611daf0e2dad1d92497f50320e534f3fc
lbilde <ljb@easv.dk>
2018-03-19 00:56:09 +0100

    adding folder-columns

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileDetailsComponent } from './file-details/file-details.component';
 import { FileSystemContainerComponent } from './file-system-container/file-system-container.component';
-import { MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatProgressSpinnerModule, MatSpinner } from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatProgressSpinnerModule
+} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FolderDetailsComponent } from './folder-details/folder-details.component';
+import { FileSystemColumnComponent } from './file-system-column/file-system-column.component';
 
 @NgModule({
   imports: [
@@ -16,7 +19,7 @@ import { FolderDetailsComponent } from './folder-details/folder-details.componen
     MatIconModule,
     MatProgressSpinnerModule
   ],
-  declarations: [FileDetailsComponent, FileSystemContainerComponent, FolderDetailsComponent],
+  declarations: [FileDetailsComponent, FileSystemContainerComponent, FolderDetailsComponent, FileSystemColumnComponent],
   providers: [],
   exports: [FileSystemContainerComponent]
 })
