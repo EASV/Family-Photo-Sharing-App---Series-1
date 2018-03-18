1ecb32e0eea7aa4bde2ce1b76569543746be8311
lbilde <ljb@easv.dk>
2018-03-18 22:08:56 +0100

    changing models to interfaces from classes

--- src/app/file-system/file-system.module.ts
+++ src/app/file-system/file-system.module.ts
@@ -5,6 +5,7 @@ import { FileSystemContainerComponent } from './file-system-container/file-syste
 import { MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatProgressSpinnerModule, MatSpinner } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '../shared/shared.module';
+import { FolderDetailsComponent } from './folder-details/folder-details.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,7 @@ import { SharedModule } from '../shared/shared.module';
     MatIconModule,
     MatProgressSpinnerModule
   ],
-  declarations: [FileDetailsComponent, FileSystemContainerComponent],
+  declarations: [FileDetailsComponent, FileSystemContainerComponent, FolderDetailsComponent],
   providers: [],
   exports: [FileSystemContainerComponent]
 })
